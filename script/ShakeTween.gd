c99a2ca4137f6a55bb63a2619932181823e987d3
njj-abs <njj@abstractit.in>
2024-02-01 16:52:31 +0530

    Created the multiple sprite.

--- script/ShakeTween.gd
+++ script/ShakeTween.gd
@@ -11,29 +11,8 @@ const TWEEN_DURATION = 0.05
 const RECOVERY_FACTOR = 2.0/3
 const TRANSITION_TYPE = Tween.TRANS_SINE
 
-var x = x_max
-var r = r_max
 func _ready():
 	host_sprite = $Sprite2D
-	
-	while x> STOP_THRESHOLD:
-		var tween = _tile_left(x,r)
-		await tween.finished
-		tween.tween_callback(queue_free)
-		print(x)
-		x *= RECOVERY_FACTOR
-		r *= RECOVERY_FACTOR
-		
-		_recenter()
-		
-		tween = _tile_right(x,r)
-		await tween.finished
-		tween.tween_callback(queue_free)
-		x *= RECOVERY_FACTOR
-		r *= RECOVERY_FACTOR
-		
-		_recenter()
-
 
 func _tile_left(x, r):
 	var tween = get_parent().create_tween()
@@ -49,11 +28,11 @@ func _tile_right(x, r):
 	tween.tween_property(host_sprite, 'position:x', host_sprite.position.x+x, TWEEN_DURATION).from(host_sprite.position.x).set_trans(TRANSITION_TYPE).set_ease(Tween.EASE_OUT)
 	
 	r = r if pivot_below else -r
-	tween.tween_property(host_sprite, 'rotation_degrees', r, TWEEN_DURATION).from(0).set_trans(TRANSITION_TYPE).set_ease(Tween.EASE_OUT)
+	tween.tween_property(host_sprite, 'rotation_degrees', -r, TWEEN_DURATION).from(0).set_trans(TRANSITION_TYPE).set_ease(Tween.EASE_OUT)
 	
 	return tween
 
-func _recenter():
+func _recenter(x, r):
 	var tween = get_parent().create_tween()
 	tween.tween_property(host_sprite, 'position:x', host_sprite.position.x, TWEEN_DURATION).from(host_sprite.position.x + x).set_trans(TRANSITION_TYPE).set_ease(Tween.EASE_IN)
 	
@@ -61,3 +40,24 @@ func _recenter():
 	tween.tween_property(host_sprite, 'rotation_degrees', 0, TWEEN_DURATION).from(r).set_trans(TRANSITION_TYPE).set_ease(Tween.EASE_IN)
 	
 	return tween
+
+
+func _on_button_pressed():
+	var x = x_max
+	var r = r_max
+	while x> STOP_THRESHOLD:
+		var tween = _tile_left(x,r)
+		await tween.finished
+		tween.tween_callback(queue_free)
+		x *= RECOVERY_FACTOR
+		r *= RECOVERY_FACTOR
+		
+		_recenter(x, r)
+		
+		tween = _tile_right(x,r)
+		await tween.finished
+		tween.tween_callback(queue_free)
+		x *= RECOVERY_FACTOR
+		r *= RECOVERY_FACTOR
+		
+		_recenter(x, r)
