95b9fe9c402d853dbc96d49dd10a8e4fc5d78a85
njj-abs <njj@abstractit.in>
2024-02-05 15:15:20 +0530

    Multi Animation player by shader.

---
+++ script/ShakeAnimationPlayer.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+@onready var Sprite = load("res://Animation/sprite_2d.tscn")
+
+func _ready():
+	for i in range(1,4):
+		var sprite = Sprite.instantiate()
+		sprite.position.x = sprite.position.x + 150 * (i)
+		print(sprite.position.x, i)
+		print(i)
+		add_child(sprite)
